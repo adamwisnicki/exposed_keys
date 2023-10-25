173ea2c23c2c7b7bc1297271906ab1ebd8e99455
adamwisnicki <148901565+adamwisnicki@users.noreply.github.com>
2023-10-25 14:56:37 +0200

    Update exposed-test.py

--- exposed-test.py
+++ exposed-test.py
@@ -1,9 +1,14 @@
-from requests.auth import HTTPBasicAuth
+# This is an example of bad security practice
+# Do not hard-code your API keys in your code
+# Use environment variables or other secure methods instead
+
 import requests
 
-url = 'https://api_url'
-headers = {'Accept': 'application/json'}
-auth = HTTPBasicAuth('apikey', 'MzCelu46sLDlWMG0H/RLemUHIFPCSErb6t7YC2Jq')
-files = {'file': open('filename', 'rb')}
+# Hard-coded API key ID and secret
+API_KEY_ID = "AKIAW6RRNZ6C2RTT5BZG"
+API_KEY_SECRET = "VJL5zhbpRZZ8hpZKngKxG7wNdAfLhTdUN0d2jFGp"
 
-req = requests.get(url, headers=headers, auth=auth, files=files)
+# Example API request using the hard-coded key
+response = requests.get("https://example.com/api/v1/data", auth=(API_KEY_ID, API_KEY_SECRET))
+data = response.json()
+print(data)
